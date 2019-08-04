68e3e3733e50cb2dc415fd8e23946148bbcd076f
ndo@ndo3 <yo@ni.co.de>
2019-08-04 23:21:00 +0200

    updated ABOUTME text + terminal header bar

--- dist/js/terminal.js
+++ dist/js/terminal.js
@@ -1,185 +1,96 @@
-/*! terminal.js v2.0 | (c) 2014 Erik Österberg | https://github.com/eosterberg/terminaljs */
+const Typer = {
+  text: null,
+  accessCountimer: null,
+  index: 0,
+  speed: 2,
+  file: '',
+  accessCount: 0,
+  deniedCount: 0,
+  init: function () {
+    accessCountimer = setInterval(function () { Typer.updLstChr() }, 500)
+    $.get(Typer.file, function (data) {
+      Typer.text = data
+      Typer.text = Typer.text.slice(0, Typer.text.length - 1)
+    })
+  },
 
-var Terminal = (function () {
-	// PROMPT_TYPE
-	var PROMPT_INPUT = 1, PROMPT_PASSWORD = 2, PROMPT_CONFIRM = 3
+  content: function () {
+    return $('#console').html()
+  },
 
-	var fireCursorInterval = function (inputField, terminalObj) {
-		var cursor = terminalObj._cursor
-		setTimeout(function () {
-			if (inputField.parentElement && terminalObj._shouldBlinkCursor) {
-				cursor.style.visibility = cursor.style.visibility === 'visible' ? 'hidden' : 'visible'
-				fireCursorInterval(inputField, terminalObj)
-			} else {
-				cursor.style.visibility = 'visible'
-			}
-		}, 500)
-	}
+  write: function (str) {
+    $('#console').append(str)
+    return false
+  },
 
-	var firstPrompt = true;
-	promptInput = function (terminalObj, message, PROMPT_TYPE, callback) {
-		var shouldDisplayInput = (PROMPT_TYPE === PROMPT_INPUT)
-		var inputField = document.createElement('input')
+  addText: function (key) {
+    if (key.keyCode === 18) {
+      Typer.accessCount++
 
-		inputField.style.position = 'absolute'
-		inputField.style.zIndex = '-100'
-		inputField.style.outline = 'none'
-		inputField.style.border = 'none'
-		inputField.style.opacity = '0'
-		inputField.style.fontSize = '0.2em'
+      if (Typer.accessCount >= 3) {
+        Typer.makeAccess()
+      }
+    } else if (key.keyCode === 20) {
+      Typer.deniedCount++
 
-		terminalObj._inputLine.textContent = ''
-		terminalObj._input.style.display = 'block'
-		terminalObj.html.appendChild(inputField)
-		fireCursorInterval(inputField, terminalObj)
+      if (Typer.deniedCount >= 3) {
+        Typer.makeDenied()
+      }
+    } else if (key.keyCode === 27) {
+      Typer.hidepop()
+    } else if (Typer.text) {
+      const cont = Typer.content()
+      if (cont.substring(cont.length - 1, cont.length) === '|') { $('#console').html($('#console').html().substring(0, cont.length - 1)) }
+      if (key.keyCode !== 8) {
+        Typer.index += Typer.speed
+      } else {
+        if (Typer.index > 0) { Typer.index -= Typer.speed }
+      }
+      const text = Typer.text.substring(0, Typer.index)
+      const rtn = new RegExp('\n', 'g')
 
-		if (message.length) terminalObj.print(PROMPT_TYPE === PROMPT_CONFIRM ? message + ' (y/n)' : message)
+      $('#console').html(text.replace(rtn, '<br/>'))
+      window.scrollBy(0, 50)
+    }
 
-		inputField.onblur = function () {
-			terminalObj._cursor.style.display = 'none'
-		}
+    if (key.preventDefault && key.keyCode != 122) {
+      key.preventDefault()
+    };
 
-		inputField.onfocus = function () {
-			inputField.value = terminalObj._inputLine.textContent
-			terminalObj._cursor.style.display = 'inline'
-		}
+    if (key.keyCode != 122) { // otherway prevent keys default behavior
+      key.returnValue = false
+    }
+  },
 
-		terminalObj.html.onclick = function () {
-			inputField.focus()
-		}
+  updLstChr: function () {
+    const cont = this.content()
 
-		inputField.onkeydown = function (e) {
-			if (e.which === 37 || e.which === 39 || e.which === 38 || e.which === 40 || e.which === 9) {
-				e.preventDefault()
-			} else if (shouldDisplayInput && e.which !== 13) {
-				setTimeout(function () {
-					terminalObj._inputLine.textContent = inputField.value
-				}, 1)
-			}
-		}
-		inputField.onkeyup = function (e) {
-			if (PROMPT_TYPE === PROMPT_CONFIRM || e.which === 13) {
-				terminalObj._input.style.display = 'none'
-				var inputValue = inputField.value
-				if (shouldDisplayInput) terminalObj.print(inputValue)
-				terminalObj.html.removeChild(inputField)
-				if (typeof(callback) === 'function') {
-					if (PROMPT_TYPE === PROMPT_CONFIRM) {
-						callback(inputValue.toUpperCase()[0] === 'Y' ? true : false)
-					} else callback(inputValue)
-				}
-			}
-		}
-		if (firstPrompt) {
-			firstPrompt = false
-			setTimeout(function () { inputField.focus()	}, 50)
-		} else {
-			inputField.focus()
-		}
-	}
+    if (cont.substring(cont.length - 1, cont.length) == '|') { $('#console').html($('#console').html().substring(0, cont.length - 1)) }
+    // else { this.write('|') } // else write it
+  }
+}
 
-	var terminalBeep
+function replaceUrls (text) {
+  const http = text.indexOf('http://')
+  const space = text.indexOf('.me ', http)
 
-	var TerminalConstructor = function (id) {
-		if (!terminalBeep) {
-			terminalBeep = document.createElement('audio')
-			var source = '<source src="http://www.erikosterberg.com/terminaljs/beep.'
-			terminalBeep.innerHTML = source + 'mp3" type="audio/mpeg">' + source + 'ogg" type="audio/ogg">'
-			terminalBeep.volume = 0.05
-		}
+  if (space !== -1) {
+    const url = text.slice(http, space - 1)
+    return text.replace(url, '<a href="' + url + '">' + url + '</a>')
+  } else {
+    return text
+  }
+}
 
-		this.html = document.createElement('div')
-		this.html.className = 'Terminal'
-		if (typeof(id) === 'string') { this.html.id = id }
+Typer.speed = 9
+Typer.file = 'ndo.txt' // add your own name here
+Typer.init()
 
-		this._innerWindow = document.createElement('div')
-		this._output = document.createElement('p')
-		this._inputLine = document.createElement('span') //the span element where the users input is put
-		this._cursor = document.createElement('span')
-		this._input = document.createElement('p') //the full element administering the user input, including cursor
+const timer = setInterval('t();', 30)
+function t () {
+  Typer.addText({ keyCode: 123748 })
 
-		this._shouldBlinkCursor = true
-
-		this.beep = function () {
-			terminalBeep.load()
-			terminalBeep.play()
-		}
-
-		this.print = function (message) {
-			var newLine = document.createElement('div')
-			newLine.textContent = message
-			this._output.appendChild(newLine)
-		}
-
-		this.input = function (message, callback) {
-			promptInput(this, message, PROMPT_INPUT, callback)
-		}
-
-		this.password = function (message, callback) {
-			promptInput(this, message, PROMPT_PASSWORD, callback)
-		}
-
-		this.confirm = function (message, callback) {
-			promptInput(this, message, PROMPT_CONFIRM, callback)
-		}
-
-		this.clear = function () {
-			this._output.innerHTML = ''
-		}
-
-		this.sleep = function (milliseconds, callback) {
-			setTimeout(callback, milliseconds)
-		}
-
-		this.setTextSize = function (size) {
-			this._output.style.fontSize = size
-			this._input.style.fontSize = size
-		}
-
-		this.setTextColor = function (col) {
-			this.html.style.color = col
-			this._cursor.style.background = col
-		}
-
-		this.setBackgroundColor = function (col) {
-			this.html.style.background = col
-		}
-
-		this.setWidth = function (width) {
-			this.html.style.width = width
-		}
-
-		this.setHeight = function (height) {
-			this.html.style.height = height
-		}
-
-		this.blinkingCursor = function (bool) {
-			bool = bool.toString().toUpperCase()
-			this._shouldBlinkCursor = (bool === 'TRUE' || bool === '1' || bool === 'YES')
-		}
-
-		this._input.appendChild(this._inputLine)
-		this._input.appendChild(this._cursor)
-		this._innerWindow.appendChild(this._output)
-		this._innerWindow.appendChild(this._input)
-		this.html.appendChild(this._innerWindow)
-
-		this.setBackgroundColor('black')
-		this.setTextColor('white')
-		this.setTextSize('1em')
-		this.setWidth('100%')
-		this.setHeight('100%')
-
-		this.html.style.fontFamily = 'Monaco, Courier'
-		this.html.style.margin = '0'
-		this._innerWindow.style.padding = '10px'
-		this._input.style.margin = '0'
-		this._output.style.margin = '0'
-		this._cursor.style.background = 'white'
-		this._cursor.innerHTML = 'C' //put something in the cursor..
-		this._cursor.style.display = 'none' //then hide it
-		this._input.style.display = 'none'
-	}
-
-	return TerminalConstructor
-}())
+  if (Typer.index > Typer.text.length) {
+    clearInterval(timer)
+  }
+}
