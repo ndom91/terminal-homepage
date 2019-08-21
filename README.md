ab33ab881dd4d9135e452c92ce9d4214557aed7c
ndo@ndo3 <yo@ni.co.de>
2019-08-22 00:15:17 +0200

    init commit: preact exampel

---
+++ README.md
@@ -0,0 +1,1 @@
+https://github.com/dangodev/pika-web-preact
--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,8 @@
-import { h, render } from "./web_modules/preact.js";
-import App from "./components/App.js";
-var appMount = document.querySelector('#app'); // @ts-ignore
-
-if (appMount) render(h(App, null), appMount);
+import { h, render } from 'preact';
+import App from './components/App';
+var appMount = document.querySelector('#app');
+// @ts-ignore
+if (appMount)
+    render(h(App, null), appMount);
 export default App;
+//# sourceMappingURL=index.js.map
