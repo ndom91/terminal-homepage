a99733a42f81866a638424360b1ae789e52c0c9b
ndo@ndo3 <yo@ni.co.de>
2019-08-29 23:33:16 +0200

    fix: terminal prompt

--- src/components/Console/text.ts
+++ src/components/Console/text.ts
@@ -1,6 +1,6 @@
 const text:string = `
-<span id="a">ndo@ndo1</span>:<span id="b">~</span><span id="c">$</span> cd Documents/<!-- lag -->About
-<span id="a">ndo@ndo1</span>:<span id="b">~</span><span id="c">$</span> cat <!-- lag -->ndo.txt
+<span id="a">ndo@ndo1</span>(<span id="b">~</span>)<span id="c">$</span> cd Documents/<!-- lag -->About
+<span id="a">ndo@ndo1</span>(<span id="b">About</span>)<span id="c">$</span> cat <!-- lag -->ndo.txt
 <p>Welcome to my cli portfolio.
 <!-- laglaglaglaglaglaglag -->
 Below are some of my most recent projects, as well as links to more info about me
@@ -9,22 +9,22 @@ I was a Sys Admin in a datacenter for a few years, before moving into my current
 <!-- delaydelaydelaydelaydelaydelay -->
 You can find some of my open source projects here:
 <!-- moredelaymoredelaymoredelaymoredelay-->
-<span id="a">ndo@ndo1</span>:<span id="b">~</span><span id="c">$</span> cat <!-- laglaglaglag -->vacation.txt
+<span id="a">ndo@ndo1</span>(<span id="b">About</span>)<span id="c">$</span> cat <!-- laglaglaglag -->vacation.txt
 
 Company Vacation Application
 Repo: <a href="https://github.com/ndom91/CompanyVacations">ndom91/CompanyVacations</a>
 <!-- delaydelaydelaydelaydelaydelaydelay -->
-<span id="a">ndo@ndo1</span>:<span id="b">~</span><span id="c">$</span> cat <!-- laglaglaglag -->maintenance.txt
+<span id="a">ndo@ndo1</span>(<span id="b">About</span>)<span id="c">$</span> cat <!-- laglaglaglag -->maintenance.txt
 
 Network Maintenance Management Application
 Repo: <a href="https://github.com/ndom91/NewtelcoMaintenance">ndom91/NewtelcoMaintenance</a>
 <!-- delaydelaydelaydelaydelaydelaydelay -->
-<span id="a">ndo@ndo1</span>:<span id="b">~</span><span id="c">$</span> cat <!-- laglaglaglag -->noc.txt
+<span id="a">ndo@ndo1</span>(<span id="b">About</span>)<span id="c">$</span> cat <!-- laglaglaglag -->noc.txt
 
 NOC Dashboard
 Repo: <a href="https://github.com/ndom91/nt-noc-dashboard">ndom91/nt-noc-dashboard</a>
 <!-- delaydelaydelaydelay -->
-<span id="a">ndo@ndo1</span>:<span id="b">~</span><span id="c">$</span> cat social.txt
+<span id="a">ndo@ndo1</span>(<span id="b">About</span>)<span id="c">$</span> cat social.txt
 
 As well as some of my online profiles:<br> 
 <a href="https://github.com/ndom91">Github</a>
